5319439ab818a41ce5fe53d4ab6b6fa37bdcce0d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-01-07 12:27:05 +0000

    Fix issue with pause menu controls

--- src/GameStates/GamePaused.lua
+++ src/GameStates/GamePaused.lua
@@ -10,6 +10,14 @@ function game_paused.exit()
     game_objects = nil
 end
 
+function game_paused.keyreleased(key)
+    if key == "return" then
+        GameState.set_state("game")
+    elseif key == "escape" then
+        love.event.quit()
+    end
+end
+
 function game_paused.draw()
     for _, o in pairs(game_objects) do
         if type(o) == "table" and o.draw then
